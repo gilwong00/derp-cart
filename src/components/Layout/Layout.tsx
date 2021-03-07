818f06cfbe9d828a08c8da830557881be34fdd26
Gilbert <gilwong3105@gmail.com>
2021-03-06 23:10:23 -0800

    Add more setup

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Layout.module.scss';
+
+const Layout = () => {
+  return (
+    <div className={styles.container}>
+
+    </div>
+  )
+}
+
+export default Layout
